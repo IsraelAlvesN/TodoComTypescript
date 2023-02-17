eaceeb63422028613f2456943395f3c39447c23c
Israel Alves <devisrael98@gmail.com>
2023-02-17 12:23:27 -0300

    Estilização dos elementos e mostrar items da lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import * as C from './App.styles'
+import { useState } from "react";
+import * as C from "./App.styles";
+import { ListItem } from "./components/ListItem";
+import { Item } from "./types/Item";
 
 const App = () => {
-  return(
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: "Comprar pão",
+      done: false,
+    },
+    {
+      id: 2,
+      name: "Comprar Café",
+      done: true,
+    },
+  ]);
+
+  return (
     <C.Container>
-      Iniciando
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        {/* Adicionar nova tarefa */}
+        {list.map((item, index) => (
+          <ListItem key={index} item={item}/>
+        ))}
+      </C.Area>
     </C.Container>
-  )
-}
+  );
+};
 
 export default App;
